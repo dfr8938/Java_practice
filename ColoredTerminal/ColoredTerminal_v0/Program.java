76998237e46800d261b455193e3ff4ed8c40f043
Denis Frolov <d.fr8938@gmail.com>
2020-06-29 20:48:23 +0300

    add colored terminal

---
+++ ColoredTerminal/ColoredTerminal_v0/Program.java
@@ -0,0 +1,13 @@
+import com.diogonunes.jcdp.color.ColoredPrinter;
+import com.diogonunes.jcdp.color.api.Ansi.BColor;
+import com.diogonunes.jcdp.color.api.Ansi.FColor;
+import com.diogonunes.jcdp.color.api.Ansi.Attribute;
+
+class Program {
+	public static void main(String[] args) {
+		ColoredPrinter cp = new ColoredPrinter.Builder(1, false).foreground(FColor.WHITE).background(BColor.BLUE).build();
+        cp.setAttribute(Attribute.REVERSE);
+		cp.println("Hello!");
+		cp.clear();
+	}
+}
