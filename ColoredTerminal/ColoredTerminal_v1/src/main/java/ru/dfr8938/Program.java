ec6b8a2eb6a901ccb05009629170fa1f38c2d182
Denis Frolov <d.fr8938@gmail.com>
2020-06-30 00:06:35 +0300

    modify ColoredTerminal_v1

--- ColoredTerminal/ColoredTerminal_v1/src/main/java/ru/dfr8938/Program.java
+++ ColoredTerminal/ColoredTerminal_v1/src/main/java/ru/dfr8938/Program.java
@@ -1,15 +1,15 @@
 package ru.dfr8938;
 
-// import com.diogonunes.jcdp.color.ColoredPrinter;
-// import com.diogonunes.jcdp.color.api.Ansi.BColor;
-// import com.diogonunes.jcdp.color.api.Ansi.FColor;
-// import com.diogonunes.jcdp.color.api.Ansi.Attribute;
+import com.diogonunes.jcdp.color.ColoredPrinter;
+import com.diogonunes.jcdp.color.api.Ansi.BColor;
+import com.diogonunes.jcdp.color.api.Ansi.FColor;
+import com.diogonunes.jcdp.color.api.Ansi.Attribute;
 
 class Program {
 	public static void main(String[] args) {
-		// ColoredPrinter cp = new ColoredPrinter.Builder(1, false).foreground(FColor.WHITE).background(BColor.BLUE).build();
-  		// cp.setAttribute(Attribute.REVERSE);
-		// cp.println("Hello!");
-		// cp.clear();
+		ColoredPrinter cp = new ColoredPrinter.Builder(1, false).foreground(FColor.WHITE).background(BColor.BLUE).build();
+  		cp.setAttribute(Attribute.REVERSE);
+		cp.println("Hello!");
+		cp.clear();
 	}
 }
